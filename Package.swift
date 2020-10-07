ff4d2200c1eae7f07d1d2d4963dad64870e775a3
Jason Leitao <jason.leitao@nomadfalcon.pt>
2020-10-07 16:50:18 +0100

    removed unused dependencies

--- Package.swift
+++ Package.swift
@@ -15,17 +15,13 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from:"4.0.0"),
-        .package(url: "https://github.com/MihaelIsaev/XMLParsing.git", from: "0.1.0"),
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.0.0")
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "Braintree",
                 dependencies: [
-                    .product(name: "Vapor", package: "vapor"),
-                    .product(name: "XMLParsing", package: "XMLParsing"),
-                    .product(name: "SWXMLHash", package: "SWXMLHash")
+                    .product(name: "Vapor", package: "vapor")
                     ]),
         .testTarget(name: "BraintreeTests", dependencies: ["Braintree"]),
     ]
